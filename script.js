b1ec527d5f9625f0c6907dc0e1f713fd4c8086fc
Simonfurlan <80962894+Simonfurlan@users.noreply.github.com>
2023-07-17 13:08:34 +0200

    Preload Bugfix

--- script.js
+++ script.js
@@ -1021,7 +1021,7 @@ function preLoad() {
 
 //Startscreen
 var startScreen = document.getElementById('startScreen');
-startScreen.style.visibility = 'hidden';
+startScreen.style.visibility = 'visible';
 preLoad();
 initialize();
 
